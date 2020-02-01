2bbb532adaddc878fd6d01f1b4f91c65f51344a1
Davis JR <davisjr2000@hotmail.com>
2020-01-31 18:54:37 -0600

    Store User on Firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,8 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-
-
 const config = {
     apiKey: "AIzaSyCA2gqATEYVxgxqlfmojZmOr6i6MLiskZc",
     authDomain: "crwn-clothing-48133.firebaseapp.com",
@@ -12,8 +10,35 @@ const config = {
     storageBucket: "crwn-clothing-48133.appspot.com",
     messagingSenderId: "224387831953",
     appId: "1:224387831953:web:d650faefcd214825d00730"
-  }
+}
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    console.log(userAuth)
+    if (!userAuth) return;
+    console.log('Now here!')
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('Error creating User', error.message);
+        }
+    }
+
+    return userRef;
+}
+ 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
