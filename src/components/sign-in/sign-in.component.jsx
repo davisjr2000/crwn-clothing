32a9e416f9078de8e773b616c8da2c61b7524794
Davis JR <davisjr2000@hotmail.com>
2020-03-28 17:01:57 -0500

    Fix h2

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -42,7 +42,7 @@ class SignIn extends React.Component{
     render(){
         return(
             <div className="sign-in">
-                <h2>I already have an account</h2>
+                <h2 class="title">I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 
                 <form onSubmit={this.handleSubmit}>
